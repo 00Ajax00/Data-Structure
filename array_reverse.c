2756053c0c0a57f649b1b5a30f6428fa008f19c2
Ajay <141296892+00Ajax00@users.noreply.github.com>
2025-03-21 09:40:25 +0000

    pushed

--- array_reverse.c
+++ array_reverse.c
@@ -1,25 +1,29 @@
-import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		
-		for(int i=0;i<n;i++){
-		    arr[i] = sc.nextInt();
-		}
-		
-		int left = 0;
-		int right = arr.length - 1;
-		
-		while(left < right){
-		    int temp = arr[left];
-		    arr[left] = arr[right];
-		    arr[right] = temp;
-		    left++;
-		    right--;
-		}
-		System.out.println(Arrays.toString(arr));
-	}
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int left = 0;
+    int right = n - 1;
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);  
+    int arr[n]; 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    reverseArray(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n"); 
+    
+    return 0;
 }
